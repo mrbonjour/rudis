d31923e7f38c938a3a63e1a371645cedb791fc85
mrbonjour <47561965+mrbonjour@users.noreply.github.com>
2022-03-28 19:36:02 +0200

    rudisv0068beta

--- main.js
+++ main.js
@@ -580,7 +580,7 @@ var gr1=getRandomInt(60);
 		{
             if(player_x+64<this.controlledactor.x+64)
 			spawnActor('EnemyArcherArrow' + this.controlledactor.name, this.controlledactor.x, this.controlledactor.y+54, 1,"fireL.png", "Projectile", null , 5000, -100, 0);   //actor 0
-            if(player_x+64>this.controlledactor.x+64)
+            if(player_x+64>=this.controlledactor.x+64)
 			spawnActor('EnemyArcherArrow' + this.controlledactor.name, this.controlledactor.x, this.controlledactor.y+54, 1,"fireR.png", "Projectile", null , 5000, 100, 0);   //actor 0 	
 		}
 	}
